13fe7a53303d77a5d57630d8b77339a13144133b
Solar Sailor <solarsailorneo@protonmail.com>
2022-06-08 13:20:08 -0400

    restructured directory to only include readme and contract

---
+++ TestCoin.sol
@@ -0,0 +1,17 @@
+// contracts/TestCoin.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract TestCoin is ERC20 {
+    constructor(uint256 totalSupply) ERC20("testCoin", "TEST") {
+        _mint(msg.sender, totalSupply);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 16;
+    }
+}
+
